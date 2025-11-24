0ebc9749346e4c67c8674e2542c0e0db48bc39df
sneghalingadurai <107161270+sneghalingadurai@users.noreply.github.com>
2025-11-24 19:30:51 +0530

    Create UserProfileServiceDeskApplication.java

---
+++ User-Profile-Service/src/main/java/com/persona/desk/UserProfileServiceDeskApplication.java
@@ -0,0 +1,13 @@
+package com.persona.desk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProfileServiceDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserProfileServiceDeskApplication.class, args);
+	}
+
+}
