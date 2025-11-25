da05c7933d88daa1004fb1636cbac0829aebbc07
sneghalingadurai <107161270+sneghalingadurai@users.noreply.github.com>
2025-11-25 18:56:37 +0530

    Update app-routing.module.ts

--- PersonaDesk-Frontend/persona-desk/src/app/app-routing.module.ts
+++ PersonaDesk-Frontend/persona-desk/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { HomeDashboardComponent } from './components/home-dashboard/home-dashboard.component';
 
 const routes: Routes = [
-  {path : '', component : HomeComponent}
+  {path : '', component : HomeComponent},
+  {path: 'dashboard', component: HomeDashboardComponent}
 ];
 
 @NgModule({
